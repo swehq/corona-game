da6effca43737d4c4e4f4f0e969a454f4d3a07f1
Jaroslav Soldát <jaroslav.soldat@swehq.com>
2020-12-19 19:47:14 +0100

    game desk layout

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {GameModule} from './game/game.module';
 import {SharedModule} from './shared/shared.module';
 
 @NgModule({
@@ -14,6 +15,7 @@ import {SharedModule} from './shared/shared.module';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedModule,
+    GameModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

b513c5bb71cd778bc1d44bb940d13e317c5aded4
Michal Benes <michal.benes@wonderland.cz>
2020-12-21 13:01:55 -0500

    Update the set of mitigations, retune simulation

--- frontend/src/app/services/game.ts
+++ frontend/src/app/services/game.ts
@@ -1,7 +1,7 @@
 import {cloneDeep, last} from 'lodash';
 import {EventHandler} from './events';
 import {MitigationEffect, Simulation} from './simulation';
-import {normalPositiveSampler} from './utils';
+import {normalSampler, normalPositiveSampler, nextDay} from './utils';
 
 interface Mitigation {
   id: string;
@@ -17,6 +17,15 @@ type MitigationState = Record<string, { active: boolean }>;
 export class Game {
   readonly startDate = '2020-03-01';
   readonly endDate = '2021-07-01';
+  readonly vaccinationStartDate = '2021-03-01';
+  readonly vaccinationPerDay = 0.01;
+
+  // Some mitigations superseed other mitigations, only the most aggresive should be turned on
+  readonly mitigationExclusivity = [
+    ["events10", "events100", "events1000"],
+    ["businessesMost", "businessesSome"],
+    ["schools", "universities"]
+  ];
 
   simulation = new Simulation(this.startDate);
   eventHandler = new EventHandler();
@@ -40,7 +49,8 @@ export class Game {
     const oldMitigationState = last(this.mitigationStates);
     if (!oldMitigationState) throw new Error('Absent mitigation');
 
-    const mitigationEffect = this.calcMitigationEffect(oldMitigationState);
+    this.sanitizeMitigationState(oldMitigationState);
+    const mitigationEffect = this.calcMitigationEffect(oldMitigationState, nextDay(this.lastDate));
     const dayState = this.simulation.simOneDay(mitigationEffect);
     const dayStats = dayState.stats;
 
@@ -58,59 +68,94 @@ export class Game {
     return this.simulation.getLastStats();
   }
 
-  get lastDate() {
+  get lastDate(): string {
     const lastState = last(this.simulation.modelStates);
-    return lastState?.date;
+    if (!lastState) throw new Error('Absent model state');
+
+    return lastState.date;
   }
 
-  isFinished() {
+  isFinished(): boolean {
     const lastState = last(this.simulation.modelStates);
-    return Boolean(this.lastDate && this.lastDate >= this.endDate);
+    return this.lastDate >= this.endDate;
   }
 
   getMitigationState() {
     return last(this.mitigationStates);
   }
 
-  calcMitigationEffect(mitigationState: MitigationState): MitigationEffect {
+  sanitizeMitigationState(mitigationState: any) {
+    this.mitigationExclusivity.forEach(ids => {
+      let active = false;
+      ids.forEach(id => {
+        if (active) mitigationState[id].active = false;
+        active ||= mitigationState[id].active;
+      });
+    });
+  }
+
+  calcMitigationEffect(mitigationState: any, forDate: string) {
     let mult = 1.0;
     let cost = 0;
     let stabilityCost = 0;
+    let bordersClosed = false;
+    let vaccinationPerDay = this.vaccinationStartDate <= forDate ? this.vaccinationPerDay : 0;
+    let monthAsString = forDate.slice(5,7);
+    let isSchoolBreak = monthAsString == "07" || monthAsString == "08";
 
     this.allMitigations.forEach(mitigation => {
+      // Special treatment of school break
+      if (mitigation.isSchool && isSchoolBreak) {
+        if (mitigation.id == "schools") mult *= (1 - mitigation.eff);
+        return;
+      }
       if (!mitigationState[mitigation.id].active) return;
+      bordersClosed ||= mitigation.isBorders;
 
-      mult *= (1 - mitigation.effectivity);
+      mult *= (1 - mitigation.eff);
       cost += mitigation.cost;
       stabilityCost += mitigation.stabilityCost;
     });
 
-    return {mult, cost, stabilityCost};
+    return {mult, cost, stabilityCost, vaccinationPerDay, bordersClosed};
   }
 
   static randomizeMitigations() {
     const mitigations: Mitigation[] = [];
 
-    const es = normalPositiveSampler(1, 0.2); // Efficiency scaler
+    const ens = normalSampler(0, 0); // FIXME: Effectivity randomness turned off during testing
     const cs = normalPositiveSampler(4e6, 0.5e6); // Cost scaler
     const ss = normalPositiveSampler(0.007, 0.002); // Stability
 
-    addMitigation('faceMasks', 0.30 * es(), 10 * cs(), 2 * ss(), 'Roušky');
-    addMitigation('distancing', 0.23 * es(), 15 * cs(), 2 * ss(), 'Rozestupy');
-    addMitigation('schools', 0.08 * es(), 50 * cs(), 15 * ss(), 'Zavřít školy');
-    addMitigation('restaurants', 0.10 * es(), 25 * cs(), 10 * ss(), 'Restaurace');
-    addMitigation('bars', 0.12 * es(), 20 * cs(), 5 * ss(), 'Zavřít bary');
-    addMitigation('travel', 0.07 * es(), 30 * cs(), 10 * ss(), 'Zavřít hranice');
-    addMitigation('eventsSome', 0.12 * es(), 20 * cs(), 5 * ss(), 'Omezení akcí');
-    addMitigation('eventsAll', 0.20 * es(), 30 * cs(), 10 * ss(), 'Zrušit akce');
+    // TODO Find reference?
+    // maybe? https://www.thelancet.com/journals/lancet/article/PIIS0140-6736(20)31142-9/fulltext cites 14.3% for face masks?
+    addMitigation("rrr",            0.14, [0.10, 0.18],  5 * cs(),  2 * ss(), "Ruce, Roušky, Rozestupy"); // Not from the paper
 
-    function addMitigation(id: string, effectivity: number, cost: number, stabilityCost: number, label: string) {
+    // https://science.sciencemag.org/content/early/2020/12/15/science.abd9338
+    addMitigation("events1000",     0.23,  [0.00, 0.40], 10 * cs(),  5 * ss(), "Akce 1000 lidí");
+    addMitigation("events100",      0.34,  [0.12, 0.52], 20 * cs(), 10 * ss(), "Akce 100 lidí");
+    addMitigation("events10",       0.42,  [0.17, 0.60], 40 * cs(), 20 * ss(), "Akce 10 lidí");
+    addMitigation("businessesSome", 0.18, [-0.08, 0.40], 20 * cs(), 15 * ss(), "Rizikové služby");
+    addMitigation("businessesMost", 0.27, [-0.03, 0.49], 40 * cs(), 30 * ss(), "Většina služeb");
+    addMitigation("universities",   0.19,  [0.05, 0.33], 10 * cs(), 10 * ss(), "Univerzity", {isSchool: true});  // 1/2 of the effect of schools (the paper couldn't robustly predict effect of universities alone)
+    addMitigation("schools",        0.38,  [0.16, 0.54], 50 * cs(), 40 * ss(), "Všechny školy", {isSchool: true});
+    addMitigation("stayHome",       0.13, [-0.05, 0.31], 50 * cs(), 30 * ss(), "Zůstat doma");       // Marginal effect of lockdown
+
+    // Controls drift across borders
+    addMitigation("borders",        0.00, [0.00, 0.00], 10 * cs(), 10 * ss(), "Zavřít hranice", {isBorders: true});    // Not from the paper
+
+    // effectivityConfidence 2sigma confidence interval (can be asymmetric)
+    // isSchool mitigations are effective during school holidays "for free"
+    // isBorders controls epidemic drift across borders
+    function addMitigation(id: string, effectivity: number, effectivityConfidence: number[], costMPerDay: number, stabilityCost: number, label: string, flags?: any) {
       mitigations.push({
         id,
         label,
-        effectivity,
-        cost,
+        eff: effectivity + ens() * (effectivityConfidence[1]-effectivityConfidence[0]) / 4,
+        cost: costMPerDay,
         stabilityCost,
+        isSchool: flags?.isSchool == true,
+        isBorders: flags?.isBorders == true,
         eventOnly: false,
       });
     }

83c2fefcbfa50d6c793bc7a5809601775b70a86d
Jaroslav Soldát <jaroslav.soldat@swehq.com>
2020-12-31 16:33:47 +0100

    Mark wide layout by top CSS class

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
+import {BreakpointObserver} from '@angular/cdk/layout';
 import {DOCUMENT} from '@angular/common';
-import {ChangeDetectionStrategy, Component, Inject, OnDestroy, Renderer2} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Inject, OnDestroy, OnInit, Renderer2} from '@angular/core';
 import {Subject} from 'rxjs';
 import {map, takeUntil} from 'rxjs/operators';
 import {ConfigService} from './services/config.service';
@@ -10,30 +11,43 @@ import {ConfigService} from './services/config.service';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent implements OnInit, OnDestroy {
 
   private destroyed = new Subject();
 
   constructor(
-    configService: ConfigService,
-    @Inject(DOCUMENT) document: Document,
-    renderer: Renderer2,
+    private configService: ConfigService,
+    @Inject(DOCUMENT) private document: Document,
+    private renderer: Renderer2,
+    private breakpointObserver: BreakpointObserver,
   ) {
-    const {body} = document;
+  }
 
-    renderer.addClass(body, 'mat-app-background');
+  ngOnInit() {
+    const {body} = this.document;
 
-    configService.config$.pipe(
+    this.renderer.addClass(body, 'mat-app-background');
+
+    const markBodyWithClass = (className: string) => (mark: boolean | undefined) => {
+      if (mark) {
+        this.renderer.addClass(body, className);
+      } else {
+        this.renderer.removeClass(body, className);
+      }
+    };
+
+    this.configService.config$.pipe(
       map(config => config.themeIsLight),
       takeUntil(this.destroyed),
     ).subscribe(
-      isLightTheme => {
-        if (isLightTheme) {
-          renderer.addClass(body, 'theme-alternate');
-        } else {
-          renderer.removeClass(body, 'theme-alternate');
-        }
-      },
+      markBodyWithClass('theme-alternate'),
+    );
+
+    this.breakpointObserver.observe(['only screen and (min-width: 768px)']).pipe(
+      map(state => state.matches),
+      takeUntil(this.destroyed),
+    ).subscribe(
+      markBodyWithClass('media-wide'),
     );
   }
 

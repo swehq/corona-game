31ecbe5fb9abe16797e24a31a0562b0ed998a1c2
Jan Walter <jnwltr@gmail.com>
2021-01-03 12:52:20 +0100

    Pass gcloud key securely to login

--- backend/src/game/routes.ts
+++ backend/src/game/routes.ts
@@ -1,36 +1,51 @@
-import express from 'express';
+import express, { IRouterMatcher } from 'express';
 import {GameDataModel} from './model';
 import {validateGame} from '../../../frontend/src/app/services/validate';
 
-
 export const router = express.Router();
 
-// router.get('/api/todo', async (req, res) => {
-//   const todos = await GameDataModel.find().exec();
-//   res.send(todos);
-// });
-// router.get('/api/todo/:id', async (req, res) => {
-//   const todo = await GameDataModel.findOne({_id: req.params.id}).exec();
-//   if (!todo) return res.sendStatus(404);
-//   res.send(todo);
-// });
-// router.put('/api/todo/:id', async (req, res) => {
+router.get('/api/game-data', async (req, res) => {
+  const data = await GameDataModel.find().exec();
+  res.send(data);
+});
+
+router.get('/api/game-data/:id', async (req, res) => {
+  const data = await GameDataModel.findOne({_id: req.params.id}).exec();
+  if (!data) return res.sendStatus(404);
+
+  res.send(data);
+});
+
+router.post('/api/game-data', async (req, res) => {
+  if (!validate(req.body)) return res.status(400).send(genericError());
+
+  const data = new GameDataModel(req.body);
+  const saveResult = await data.save();
+
+  if (saveResult.errors) return res.status(400).send(genericError());
+
+  res.send({id: data._id});
+});
+
+// TODO implement and use on FE or remove
+// router.put('/api/game-data/:id', async (req, res) => {
 //   const todo = await GameDataModel.findOneAndUpdate({_id: req.params.id}, req.body, {new: true}).exec();
 //   if (!todo) return res.sendStatus(404);
 //   res.send(todo);
 // });
 
-router.post('/api/game-data', async (req, res) => {
+function genericError() {
+  return {error: 'Game data invalid'};
+}
+
+function validate(data: any): boolean {
+  if (!data.simulation && !data.simulation.length) return false;
+
   try {
-    if (!validateGame(req.body)) return res.status(400).send({error: 'Game data invalid'});
+    if (!validateGame(data)) return false;
   } catch (e) {
-    return res.status(400).send({error: 'Game data invalid'});
+    return false;
   }
 
-  const data = new GameDataModel(req.body);
-  const saveResult = await data.save();
-
-  if (saveResult.errors) return res.status(400).send({error: 'Game data invalid'});
-
-  res.send({id: data._id});
-});
+  return true;
+}

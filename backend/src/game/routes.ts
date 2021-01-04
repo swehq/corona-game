4b6bfacaf97e8868da34e55d51c8d9e1e2962229
Pavel Zemek <pavel@zemek.eu>
2021-01-04 16:03:39 +0100

    Replace Express with Koa

--- backend/src/game/routes.ts
+++ backend/src/game/routes.ts
@@ -1,49 +1,44 @@
-import express from 'express';
+import Router from 'koa-router';
 import {GameDataModel} from './model';
 import {validateGame} from '../../../frontend/src/app/services/validate';
 import {GameData} from '../../../frontend/src/app/services/game';
 import {last} from 'lodash';
 
-export const router = express.Router();
+export const router = new Router();
 
-router.get('/api/game-data', async (req, res, next) => {
-  try {
-    const data = await GameDataModel.find();
-    res.send(data.map((i: any) => i.results));
-  } catch (e) {
-    return next(e);
-  }
+router.get('/api/game-data', async (ctx) => {
+  const data = await GameDataModel.find();
+  ctx.body = data.map((i: any) => i.results);
 });
 
-router.get('/api/game-data/:id', async (req, res, next) => {
-  try {
-    const data = await GameDataModel.findOne({_id: req.params.id}).exec();
-    if (!data) return res.sendStatus(404);
-
-    res.send(data);
-  } catch (e) {
-    return next(e);
-  }
+router.get('/api/game-data/:id', async (ctx) => {
+  const data = await GameDataModel.findOne({_id: ctx.params.id}).exec();
+  if (!data) return ctx.status = 404;
+  ctx.body = data;
 });
 
-router.post('/api/game-data', async (req, res, next) => {
-  try {
-    const inputData: GameData = req.body;
-    if (!validate(inputData)) return res.status(400).send(genericError());
-
-    const lastDayData = last(inputData.simulation)!;
-    const dead = lastDayData.stats.deaths.total;
-    const cost = lastDayData.stats.costs.total;
-
-    const saveData = new GameDataModel({...inputData, results: {dead, cost}});
-    const saveResult = await saveData.save();
-
-    if (saveResult.errors) return res.status(400).send(genericError());
-
-    res.send({id: saveData._id});
-  } catch (e) {
-    return next(e);
+router.post('/api/game-data', async (ctx) => {
+  const inputData: GameData = ctx.request.body;
+  if (!validate(inputData)) {
+    ctx.status = 400;
+    ctx.body = genericError();
+    return;
   }
+
+  const lastDayData = last(inputData.simulation)!;
+  const dead = lastDayData.stats.deaths.total;
+  const cost = lastDayData.stats.costs.total;
+
+  const saveData = new GameDataModel({...inputData, results: {dead, cost}});
+  const saveResult = await saveData.save();
+
+  if (saveResult.errors) {
+    ctx.status = 400;
+    ctx.body = genericError();
+    return;
+  }
+
+  ctx.body = {id: saveData._id};
 });
 
 // TODO implement and use on FE or remove

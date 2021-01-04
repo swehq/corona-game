1c993a9d48ed623d01d4f351eb0b6bfee59d5775
Pavel Zemek <pavel@zemek.eu>
2021-01-04 15:28:39 +0100

    Handle errors in routes

--- backend/src/game/routes.ts
+++ backend/src/game/routes.ts
@@ -6,32 +6,44 @@ import {last} from 'lodash';
 
 export const router = express.Router();
 
-router.get('/api/game-data', async (req, res) => {
-  const data = await GameDataModel.find()
-  res.send(data.map((i: any) => i.results));
+router.get('/api/game-data', async (req, res, next) => {
+  try {
+    const data = await GameDataModel.find();
+    res.send(data.map((i: any) => i.results));
+  } catch (e) {
+    return next(e);
+  }
 });
 
-router.get('/api/game-data/:id', async (req, res) => {
-  const data = await GameDataModel.findOne({_id: req.params.id}).exec();
-  if (!data) return res.sendStatus(404);
+router.get('/api/game-data/:id', async (req, res, next) => {
+  try {
+    const data = await GameDataModel.findOne({_id: req.params.id}).exec();
+    if (!data) return res.sendStatus(404);
 
-  res.send(data);
+    res.send(data);
+  } catch (e) {
+    return next(e);
+  }
 });
 
-router.post('/api/game-data', async (req, res) => {
-  const inputData: GameData = req.body;
-  if (!validate(inputData)) return res.status(400).send(genericError());
+router.post('/api/game-data', async (req, res, next) => {
+  try {
+    const inputData: GameData = req.body;
+    if (!validate(inputData)) return res.status(400).send(genericError());
 
-  const lastDayData = last(inputData.simulation)!;
-  const dead = lastDayData.stats.deaths.total;
-  const cost = lastDayData.stats.costs.total;
+    const lastDayData = last(inputData.simulation)!;
+    const dead = lastDayData.stats.deaths.total;
+    const cost = lastDayData.stats.costs.total;
 
-  const saveData = new GameDataModel({...inputData, results: {dead, cost}});
-  const saveResult = await saveData.save();
+    const saveData = new GameDataModel({...inputData, results: {dead, cost}});
+    const saveResult = await saveData.save();
 
-  if (saveResult.errors) return res.status(400).send(genericError());
+    if (saveResult.errors) return res.status(400).send(genericError());
 
-  res.send({id: saveData._id});
+    res.send({id: saveData._id});
+  } catch (e) {
+    return next(e);
+  }
 });
 
 // TODO implement and use on FE or remove

ae415b945c1191946d99bcb27f88bac285202944
Jaroslav Soldát <jaroslav.soldat@swehq.com>
2021-01-16 22:06:24 +0100

    Fix Karma tests

--- frontend/src/app/game/credits/credits.component.spec.ts
+++ frontend/src/app/game/credits/credits.component.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 import {GameModule} from '../game.module';
 import {CreditsComponent} from './credits.component';
 
@@ -9,6 +10,7 @@ describe('CreditsComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule,
         GameModule,
       ],
     })
--- frontend/src/app/game/intro/intro.component.spec.ts
+++ frontend/src/app/game/intro/intro.component.spec.ts
@@ -1,6 +1,7 @@
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {RouterTestingModule} from '@angular/router/testing';
 import {GameModule} from '../game.module';
 import {IntroComponent} from './intro.component';
 
@@ -11,6 +12,7 @@ describe('IntroComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
+        RouterTestingModule,
         HttpClientTestingModule,
         NoopAnimationsModule,
         GameModule,

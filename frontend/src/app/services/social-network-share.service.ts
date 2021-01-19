57b30afdabf32545eedc50ffdb3a62f23c661247
Zdenek Hatak <zdenek@hatak.cz>
2021-01-19 12:17:04 +0100

    Add share service, add share buttons on outro page

---
+++ frontend/src/app/services/social-network-share.service.ts
@@ -0,0 +1,35 @@
+import {Injectable} from '@angular/core';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class SocialNetworkShareService {
+
+  shareFacebook(shareUrl = environment.baseUrl) {
+    const url = 'https://www.facebook.com/dialog/share?app_id=' + environment.facebookAppId + '&href=' + shareUrl;
+    window.open(url, 'name', 'height=500,width=520,top=200,left=300,resizable');
+  }
+
+  shareTwitter(msg = '', shareUrl = environment.baseUrl) {
+     let twitterShareUrl = 'http://twitter.com/share?';
+
+     const params: any = {
+       url: shareUrl,
+       text: msg,
+       hashtags: 'covid19',
+     };
+
+     for (const prop in params) twitterShareUrl += '&' + prop + '=' + encodeURIComponent(params[prop]);
+     window.open(twitterShareUrl, '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
+  }
+
+  shareLinkedIn(msg = '', shareUrl = environment.baseUrl) {
+    const url = `https://www.linkedin.com/shareArticle?mini=true
+      &url=${shareUrl}
+      &title=Korona%20Hra
+      &summary=${msg}`;
+
+    window.open(url, 'name', 'height=500,width=520,top=200,left=300,resizable');
+  }
+}

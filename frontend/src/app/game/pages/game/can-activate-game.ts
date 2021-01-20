6f558ef0a8c987db2b11855d903eca78e2026016
Petr26 <petr.koucky.26@gmail.com>
2021-01-20 09:32:38 +0100

    Add options to continue with saved game or play new

--- frontend/src/app/game/pages/game/can-activate-game.ts
+++ frontend/src/app/game/pages/game/can-activate-game.ts
@@ -1,13 +1,18 @@
 import {Injectable} from '@angular/core';
-import {CanActivate} from '@angular/router';
+import {CanActivate, Router} from '@angular/router';
 import {LayoutUtilsService} from 'src/app/services/layout-utils.service';
+import {GameService} from '../../game.service';
 
 @Injectable()
 export class CanActivateGame implements CanActivate {
-  constructor(private layoutUtils: LayoutUtilsService) {}
+  constructor(private layoutUtils: LayoutUtilsService, private router: Router, private gameService: GameService) {}
 
   canActivate() {
     this.layoutUtils.setIsGame(true);
+    if (!this.gameService.game && this.gameService.isLocalStorageGame()) {
+      this.router.navigate(['intro']);
+      return false;
+    }
     return true;
   }
 }

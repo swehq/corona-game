94a2f9700e0b9a138c76c38464a1280cf73108b0
Jan Walter <jnwltr@gmail.com>
2021-02-10 19:18:38 +0100

    Add move command for revalidated invalid games

--- backend/src/game/commands.ts
+++ backend/src/game/commands.ts
@@ -1,25 +1,85 @@
-import {validateGame} from '../../../frontend/src/app/services/validate';
-import {InvalidGameDataModel} from './model';
+import {last} from 'lodash';
 import mongoose from 'mongoose';
 import {exit} from 'process';
+import {validateGame} from '../../../frontend/src/app/services/validate';
+import {GameData} from '../app/services/game';
+import {GameDataModel, InvalidGameDataModel} from './model';
 
-async function run(): Promise<void> {
-  const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:8001/corona';
+const BATCH_SIZE = 10;
+const MONGO_URI = process.env.MONGO_URI ?? 'mongodb://localhost:8001/corona';
 
+let command: () => Promise<void>;
+switch (process.argv[2]) {
+  case 'move':
+    command = move;
+    break;
+  default:
+    command = validate;
+}
+
+command()
+  .catch(console.error)
+  .finally(exit);
+
+async function connect(): Promise<void> {
   await mongoose.connect(MONGO_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-
-  const results = await InvalidGameDataModel.find({});
-  for (const result of results) {
-    const validity = validateGame(result.data).validity;
-    result.validity = validity;
-    await result.save();
-  };
 }
 
-run()
-  .catch(console.error)
-  .finally(exit);
+async function move(): Promise<void> {
+  console.time('move');
+  await connect();
+
+  while (true) {
+    const results = await InvalidGameDataModel
+      .find({validity: 'valid'})
+      .limit(BATCH_SIZE);
+
+    if (!results.length) break;
+
+    console.log(`Processing batch of ${results.length}`);
+    console.timeLog('move');
+
+    for (const result of results) {
+      const gameData = result.data as GameData;
+      const lastDayData = last(gameData.simulation)!;
+      const dead = lastDayData.stats.deaths.total;
+      const cost = lastDayData.stats.costs.total;
+
+      const game = new GameDataModel({...gameData, created: result.created, results: {dead, cost}});
+      await game.save();
+
+      result.validity = 'valid-moved';
+      await result.save();
+    }
+  }
+
+  console.timeEnd('move');
+}
+
+async function validate(): Promise<void> {
+  console.time('validate');
+  await connect();
+
+  while (true) {
+    const results = await InvalidGameDataModel
+      .find({validity: {$exists: false}})
+      .limit(BATCH_SIZE);
+
+    if (!results.length) break;
+
+    console.log(`Processing batch of ${results.length}`);
+    console.timeLog('validate');
+
+    for (const result of results) {
+      const validity = validateGame(result.data).validity;
+      result.validity = validity;
+      await result.save();
+    };
+  }
+
+  console.timeEnd('validate');
+}

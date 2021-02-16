07920f4b52d942677a00fad7810375aa342103ac
McPenquen <majda.calk@outlook.com>
2021-02-16 19:41:48 +0100

    Remove MutationObservation cypress function

--- frontend/cypress/support/commands.ts
+++ frontend/cypress/support/commands.ts
@@ -3,7 +3,6 @@ declare namespace Cypress {
     playEvents(num: number): Chainable<Response>;
     checkEventPresence(): Chainable<Response>;
     playUntilDate(date: string): Chainable<Response>;
-    playUntilDate2(date: string): Chainable<Response>;
   }
 }
 
@@ -23,26 +22,6 @@ Cypress.Commands.add('checkEventPresence', () => {
     if ($events.children().length > 0) {
       cy.playEvents(1);
     }
-  });
-})
-
-Cypress.Commands.add('playUntilDate2', (date) => {
-  console.log('Method start');
-  cy.get('cvd-status-display > cvd-row:first-child > div > div:first-child').then($node => {
-      let observerDisconnected = false;
-      const observer = new MutationObserver(callback);
-
-      function callback(mutations: any[], observer: MutationObserver) {
-        for (const mutation of mutations) {
-          if (mutation.type === 'characterData' &&
-           mutation.target.textContent === 'Dnes je ' + date) {
-            observerDisconnected = true;
-            observer.disconnect();
-          } 
-        }
-      };
-      observer.observe($node.get(0), {characterData: true, subtree:true});
-      cy.get('[ng-reflect-svg-icon=pause]').first().click();
   });
 })
 
